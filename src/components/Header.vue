f7f1f9cc9daa3d874a606552e4dcc0eb7271fcac
TarasKosiachenko <taras.kosiachenko@gmail.com>
2021-11-16 16:33:46 +0200

    created a header with search

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="header">
+    <BNavbar type="dark" class="navbar" variant="dark">
+      <BContainer>
+        <BNavbarBrand href="#">MovieDB</BNavbarBrand>
+        <BNavForm>
+          <BFormInput
+            class="mr-sm-2 search-input"
+            placeholder="Search"
+            v-model="searchValue"
+            debounce="800"
+          ></BFormInput>
+        </BNavForm>
+      </BContainer>
+    </BNavbar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({
+    searchValue: "",
+  }),
+  watch: {
+    searchValue: "onSearchValueChanged",
+  },
+  methods: {
+    onSearchValueChanged(val) {
+      console.log(val);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  margin-bottom: 30px;
+}
+
+.navbar {
+  background-color: rgba(0, 0, 0, 0.7) !important;
+}
+
+.search-input {
+  color: #fff;
+  background: rgba(255, 255, 255, 0.1);
+  border-color: rgba(0, 0, 0, 0.6);
+}
+
+.search-input:focus {
+  box-shadow: none;
+  color: #fff;
+  background: rgba(255, 255, 255, 0.2);
+  border-color: rgba(0, 0, 0, 0.6);
+}
+.search-input:hover {
+  background: rgba(255, 255, 255, 0.2);
+  border-color: rgba(0, 0, 0, 0.6);
+}
+</style>
